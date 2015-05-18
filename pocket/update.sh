a0edf3cc9d71a2a76c9e9ec46642b5afdf6fbbe0
chainsawriot <chainsawtiney@gmail.com>
2015-05-18 18:56:01 +0800

    update

--- pocket/update.sh
+++ pocket/update.sh
@@ -3,6 +3,6 @@ Rscript generate_band.R
 Rscript sorting.R
 echo `date` | tee -a index.html
 echo '</body></html>' | tee -a index.html
-git commit -am '$1'
+git commit -am $1
 git push origin master
 
