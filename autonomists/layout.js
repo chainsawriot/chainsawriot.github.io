3fdcb0267481336e76a07430f3a81b09c3e3b9f7
chainsawriot <chainsawtiney@gmail.com>
2016-06-12 07:40:31 +0800

    autonomists wordcloud

---
+++ autonomists/layout.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+    var wordcloudrr_data = JSON.parse(data)
+    var wordcloudrr_list = []
+    for (var row in wordcloudrr_data['data']) {
+	wordcloudrr_list.push([wordcloudrr_data['data'][row]['words'], wordcloudrr_data['data'][row]['freq']])
+    }
+    var tar_wordcloudrr_data = TAFFY(wordcloudrr_data['data'])
+    function query(word, freq) {
+	return tar_wordcloudrr_data({'words': word, "freq": freq}).get()[0]['cols']
+    }
+    options= {
+	list : wordcloudrr_list,
+	rotateRatio: 0,
+	shape: wordcloudrr_data['shape'][0],
+	color: function(word, weight) {
+	    return query(word, weight)
+	}
+    }
+    WordCloud(document.getElementById('wordcloudrr'), options)
+}
